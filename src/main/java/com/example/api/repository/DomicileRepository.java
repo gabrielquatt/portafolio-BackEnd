6e5a493a82f808cff42490dc53c30e4e79d47a89
Gabriel Quattrini <63880747+gabrielquatt@users.noreply.github.com>
2022-10-13 20:48:05 -0300

    delete Domicile.java

--- src/main/java/com/example/api/repository/DomicileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.api.repository;
-
-import com.example.api.model.Domicile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DomicileRepository extends JpaRepository<Domicile, Long> {
-}
